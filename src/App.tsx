a14cfcf3b6bbfc32426c95285102036960dc20ad
pdy1207 <pdyme1207@gmail.com>
2023-09-28 21:59:33 +0900

    🆕 feat: 라우터 초기셋팅 및 글로벌 스타일제작 폰트 적용 폰트는 추후 변경될 가능성 있음!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
-import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import "./font/font.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  { path: "/create-account", element: <CreateAccount /> },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: 'nix';
+  }  
+`;
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>안녕하세요</h1>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
