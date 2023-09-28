d2447400cc3cd0dc3787d657d7f8e82b6b30a3e4
pdy1207 <pdyme1207@gmail.com>
2023-09-28 22:35:29 +0900

    🔧 build: 파이어베이스 초기셋팅

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArwAZN0Q1oI056Agbt_W7JPNRd4QilOUE",
+  authDomain: "xwitter-13454.firebaseapp.com",
+  projectId: "xwitter-13454",
+  storageBucket: "xwitter-13454.appspot.com",
+  messagingSenderId: "644285252616",
+  appId: "1:644285252616:web:a030363f79333e71053dbd",
+};
+
+// Firebase initialize
+const app = initializeApp(firebaseConfig);
