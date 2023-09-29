db3b72a83fb61f99126f2c341ccc1a00280d13e3
pdy1207 <pdyme1207@gmail.com>
2023-09-29 18:11:16 +0900

    🆕 feat: 데이터 쿼리 및 해당 타임라인 제작 완료

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,8 +1,14 @@
 import { styled } from "styled-components";
 import PostTweetForm from "../components/post-tweet-form";
 import { auth } from "../firebase";
+import Timeline from "../components/timeline";
+import "../css/scroll.css";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  grid-template-rows: 1fr 5fr;
+`;
 
 export default function Home() {
   // const logOut = () => {
@@ -11,6 +17,7 @@ export default function Home() {
   return (
     <Wrapper>
       <PostTweetForm />
+      <Timeline />
     </Wrapper>
   );
 }
